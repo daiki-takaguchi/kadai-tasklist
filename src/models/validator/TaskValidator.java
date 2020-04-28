7288d4a31325d31854ec630d5964a4a553dfe640
daiki.takaguchi <kouguchi-daiki@nihon-data.jp>
2020-04-28 14:11:09 +0900

    Add validation

---
+++ src/models/validator/TaskValidator.java
@@ -0,0 +1,27 @@
+package models.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+	public static List<String> validate(Task t) {
+		List<String> errors = new ArrayList<String>();
+
+		String content_error = _validateContent(t.getContent());
+		if(!content_error.equals("")) {
+			errors.add(content_error);
+		}
+
+		return errors;
+	}
+
+	public static String _validateContent(String content) {
+		if(content == null || content.equals("")) {
+			return "内容を入力してください";
+		}
+
+		return "";
+	}
+}
